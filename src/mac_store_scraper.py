84f011ef156340a8f7a94eb7a995d0a8e2ea1e36
Aryan Chegini <cheginiaryan.ac@gmail.com>
2022-08-19 16:24:24 +0100

    working version

--- src/mac_store_scraper.py
+++ src/mac_store_scraper.py
@@ -3,24 +3,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import warnings
 
-#ignore annoying deprecation warnings
+# ignore annoying deprecation warnings
 warnings.simplefilter("ignore")
 
 
 class MacStoreScraper:
-    def __init__(self, address:str) -> None:
+    def __init__(self, address: str) -> None:
         self._website_link = 'https://www.maccosmetics.co.uk/stores'
         self.address_given = address
-        ##driver setup
+        # driver setup
         s = Service(ChromeDriverManager().install())
-        #setting up headless chrome
-        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
+        # setting up headless chrome
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
         options = webdriver.ChromeOptions()
         options.headless = True
         options.add_argument(f'user-agent={user_agent}')
@@ -36,33 +36,36 @@ class MacStoreScraper:
         options.add_argument('--no-sandbox')
         self.driver = webdriver.Chrome(service=s, options=options)
 
-    @property      
+    @property
     def nearest_store_info(self) -> str:
         self.driver.get(self._website_link)
-        searchbox = self.driver.find_element(By.XPATH, '//*[@id="main_content"]/div[2]/div/article/div/div/div[1]/div[1]/div/div[1]/div/form/span/div[1]/input')
+        searchbox = self.driver.find_element(By.XPATH,
+                                             '//*[@id="main_content"]/div[2]/div/article/div/div/div[1]/div[1]/div/div[1]/div/form/span/div[1]/input')
         searchbox.send_keys(self.address_given)
         searchbox.submit()
-        #ensuring the program doesnt crash
+        # ensuring the program doesnt crash
         is_scraped = False
         while not is_scraped:
             try:
-                store_name_elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/article/div/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/table/tbody/tr[1]/td[2]/table/tbody/tr[1]/td[1]/h6[1]')))
+                store_name_elem = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.XPATH,
+                                                                                                       '/html/body/div[1]/div[2]/div/article/div/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/table/tbody/tr[1]/td[2]/table/tbody/tr[1]/td[1]/h6[1]')))
                 store_name = store_name_elem.text
-                store_details_elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/article/div/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/table/tbody/tr[1]/td[2]/table/tbody/tr[2]/td')))
+                store_details_elem = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.XPATH,
+                                                                                                          '/html/body/div[1]/div[2]/div/article/div/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/table/tbody/tr[1]/td[2]/table/tbody/tr[2]/td')))
                 store_details = store_details_elem.text
                 store_details_rows = store_details.split('\n')
-                
+
                 info = f'Your nearest store is {store_name}.\nIt is {store_details_rows[9]} away\nIt\'s address is {" ".join(store_details_rows[:2])}\nFeel free to give us a call at {store_details_rows[2]}\nWe look forward to seeing you!'
                 is_scraped = True
                 return info
             except Exception as exc:
                 print(f'The program crashed while attempting to scrape data:\n--> {str(exc)[:100]}\nre-running...')
                 time.sleep(1)
-                
+
     def give_store_details(self) -> None:
         details = self.nearest_store_info
         return str(details)
-    
+
     def give_store_postcode(self) -> str:
         details = self.nearest_store_info
         details_rows = details.split('\n')
--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 from mac_store_scraper import MacStoreScraper
 import openpyxl
 import pandas as pd
-import time
 import warnings
 
-#ignore annoying deprecation warnings
+# ignore annoying deprecation warnings
 warnings.simplefilter("ignore")
 
 
 class BillBoardFinder:
     def __init__(self) -> None:
-        self.excelfile = 'M.A.C Billboard Data.xlsx'
+        self.excelfile = '../M.A.C Billboard Data.xlsx'
         self.workbook = openpyxl.load_workbook(self.excelfile)
         self.worksheet = self.workbook['ATLAS BOOKED SITE LIST']
         self.df = pd.read_excel(self.excelfile)
@@ -24,19 +23,19 @@ class BillBoardFinder:
                 postcodes.append(cell.value)
         postcodes.pop(0)
         return postcodes
-    
+
     @property
     def scraper(self) -> MacStoreScraper:
         scraper = MacStoreScraper(self.billboard_postcodes[self.index])
         return scraper
-    
+
     def progress(self):
         print(f'Row number {self.index + 1} complete')
 
     def find_postcode_of_nearest(self) -> str:
         nearest_postcode = self.scraper.give_store_postcode()
         return nearest_postcode
-    
+
     def find_details_of_nearest(self) -> str:
         nearest_store_data = self.scraper.give_store_details()
         return nearest_store_data
@@ -49,38 +48,39 @@ class BillBoardFinder:
             store_postcodes.append(nearest_postcode)
             self.progress()
         self.df["Nearest M.A.C Store Postcode"] = store_postcodes
-        self.df.to_excel(r'../M.A.C Billboard Data - w Nearest Postcodes.xlsx', index = False)
+        self.df.to_excel(r'../M.A.C Billboard Data - w Nearest Postcodes.xlsx', index=False)
         return
-    
-    def insert_details(self, s_name:bool=True, s_postcode:bool=True, s_number:bool=True, s_distance_away:bool=True) -> None:
+
+    def insert_details(self, s_name: bool = True, s_postcode: bool = True, s_number: bool = True,
+                       s_distance_away: bool = True) -> None:
         store_postcodes = []
         distances_away = []
         store_tel_numbers = []
         for i in range(len(self.billboard_postcodes)):
             self.index = i
             store_details = self.find_details_of_nearest()
-                    
+
             detail_rows = store_details.split('\n')
-            #extracting postcode
+            # extracting postcode
             row_withpostcode = detail_rows[2].split(' ')
             postcode = f'{row_withpostcode[len(row_withpostcode) - 2]} {row_withpostcode[len(row_withpostcode) - 1]}'
             store_postcodes.append(postcode)
-            #extracting_distance
+            # extracting_distance
             row_withdistance = detail_rows[1].split(' ')
             distance = f'{row_withdistance[2]} {row_withdistance[3]}'
             distances_away.append(distance)
-            #extracting store telephone numbers
+            # extracting store telephone numbers
             row_with_telnumbers = detail_rows[3].split(' ')
             tel_numbers = ' '.join(row_with_telnumbers[8:])
             store_tel_numbers.append(tel_numbers)
             self.progress()
-            
-            
+
         self.df['M.A.C Store Postcode'] = store_postcodes
         self.df['M.A.C Store Tel Number'] = store_tel_numbers
         self.df['Distance Away'] = distances_away
-        self.df.to_excel(r'../M.A.C Billboard Data - w M.A.C Store Data.xlsx', index = False)
+        self.df.to_excel(r'../M.A.C Billboard Data - w M.A.C Store Data.xlsx', index=False)
         return
+
 
 sample = BillBoardFinder()
 sample.insert_details()
