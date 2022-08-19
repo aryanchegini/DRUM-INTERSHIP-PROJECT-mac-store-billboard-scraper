bfa62939fc7510e72b7bc957c3b905330c5d7b5f
Aryan Chegini <cheginiaryan.ac@gmail.com>
2022-08-19 17:36:18 +0100

    Modified the code to create a much more efficient and faster algorithm. I did this by modifying the web crawler to operate on a list of postcodes at once rather than launch a new web driver for each postcode. As a result it is much faster.

--- src/mac_store_scraper.py
+++ src/mac_store_scraper.py
@@ -1,24 +1,26 @@
-import webbrowser
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 import warnings
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 # ignore annoying deprecation warnings
 warnings.simplefilter("ignore")
 
 
 class MacStoreScraper:
-    def __init__(self, address: str) -> None:
+    def __init__(self, postcodes: list) -> None:
         self._website_link = 'https://www.maccosmetics.co.uk/stores'
-        self.address_given = address
+        self.postcodes = postcodes
+        self.index = 0
+
         # driver setup
         s = Service(ChromeDriverManager().install())
+
         # setting up headless chrome
         user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
         options = webdriver.ChromeOptions()
@@ -36,39 +38,46 @@ class MacStoreScraper:
         options.add_argument('--no-sandbox')
         self.driver = webdriver.Chrome(service=s, options=options)
 
-    @property
-    def nearest_store_info(self) -> str:
+    def get_nearest_postcodes(self) -> list:
         self.driver.get(self._website_link)
-        searchbox = self.driver.find_element(By.XPATH,
-                                             '//*[@id="main_content"]/div[2]/div/article/div/div/div[1]/div[1]/div/div[1]/div/form/span/div[1]/input')
-        searchbox.send_keys(self.address_given)
-        searchbox.submit()
-        # ensuring the program doesnt crash
-        is_scraped = False
-        while not is_scraped:
-            try:
-                store_name_elem = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.XPATH,
-                                                                                                       '/html/body/div[1]/div[2]/div/article/div/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/table/tbody/tr[1]/td[2]/table/tbody/tr[1]/td[1]/h6[1]')))
-                store_name = store_name_elem.text
-                store_details_elem = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.XPATH,
-                                                                                                          '/html/body/div[1]/div[2]/div/article/div/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/table/tbody/tr[1]/td[2]/table/tbody/tr[2]/td')))
-                store_details = store_details_elem.text
-                store_details_rows = store_details.split('\n')
+        search_box = self.driver.find_element(By.XPATH,
+                                              '//*[@id="main_content"]/div[2]/div/article/div/div/div[1]/div[1]/div/div[1]/div/form/span/div[1]/input')
 
-                info = f'Your nearest store is {store_name}.\nIt is {store_details_rows[9]} away\nIt\'s address is {" ".join(store_details_rows[:2])}\nFeel free to give us a call at {store_details_rows[2]}\nWe look forward to seeing you!'
-                is_scraped = True
-                return info
-            except Exception as exc:
-                print(f'The program crashed while attempting to scrape data:\n--> {str(exc)[:100]}\nre-running...')
-                time.sleep(1)
+        nearest_postcodes = []
 
-    def give_store_details(self) -> None:
-        details = self.nearest_store_info
-        return str(details)
+        for i in range(len(self.postcodes)):
 
-    def give_store_postcode(self) -> str:
-        details = self.nearest_store_info
+            search_box.clear()
+            search_box.send_keys(self.postcodes[i])
+            search_box.submit()
+            info = ''
+            # ensuring the program doesn't crash
+            is_scraped = False
+            while not is_scraped:
+                try:
+                    store_name_elem = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.XPATH,
+                                                                                                           '/html/body/div[1]/div[2]/div/article/div/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/table/tbody/tr[1]/td[2]/table/tbody/tr[1]/td[1]/h6[1]')))
+                    store_name = store_name_elem.text
+                    store_details_elem = WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.XPATH,
+                                                                                                              '/html/body/div[1]/div[2]/div/article/div/div/div[1]/div[2]/div[2]/div[2]/div/div[3]/table/tbody/tr[1]/td[2]/table/tbody/tr[2]/td')))
+                    store_details = store_details_elem.text
+                    store_details_rows = store_details.split('\n')
+
+                    info = f'Your nearest store is {store_name}.\nIt is {store_details_rows[9]} away\nIt\'s address is {" ".join(store_details_rows[:2])}\nFeel free to give us a call at {store_details_rows[2]}\nWe look forward to seeing you!'
+                    is_scraped = True
+                except Exception as exc:
+                    print(f'The program crashed while attempting to scrape data:\n--> {str(exc)[:100]}\nre-running...')
+                    time.sleep(1)
+
+            self.index += 1
+            print(f'Billboard {self.index} complete: {self.get_store_postcode(info)}')
+            nearest_postcodes.append(self.get_store_postcode(info))
+
+        return nearest_postcodes
+
+    @staticmethod
+    def get_store_postcode(details) -> str:
         details_rows = details.split('\n')
-        row_withpostcode = details_rows[2].split(' ')
-        postcode = f'{row_withpostcode[len(row_withpostcode) - 2]} {row_withpostcode[len(row_withpostcode) - 1]}'
+        row_with_postcode = details_rows[2].split(' ')
+        postcode = f'{row_with_postcode[len(row_with_postcode) - 2]} {row_with_postcode[len(row_with_postcode) - 1]}'
         return str(postcode)
--- src/main.py
+++ src/main.py
@@ -9,78 +9,65 @@ warnings.simplefilter("ignore")
 
 class BillBoardFinder:
     def __init__(self) -> None:
-        self.excelfile = '../M.A.C Billboard Data.xlsx'
-        self.workbook = openpyxl.load_workbook(self.excelfile)
+        self.excel_file = '../M.A.C Billboard Data.xlsx'
+        self.workbook = openpyxl.load_workbook(self.excel_file)
         self.worksheet = self.workbook['ATLAS BOOKED SITE LIST']
-        self.df = pd.read_excel(self.excelfile)
-        self.index = 0
+        self.df = pd.read_excel(self.excel_file)
 
     @property
     def billboard_postcodes(self) -> list:
         postcodes = []
         for cell in self.worksheet['H']:
-            if cell.value != None:
+            if cell.value is not None:
                 postcodes.append(cell.value)
         postcodes.pop(0)
         return postcodes
 
     @property
     def scraper(self) -> MacStoreScraper:
-        scraper = MacStoreScraper(self.billboard_postcodes[self.index])
+        scraper = MacStoreScraper(self.billboard_postcodes)
         return scraper
 
-    def progress(self):
-        print(f'Row number {self.index + 1} complete')
-
-    def find_postcode_of_nearest(self) -> str:
-        nearest_postcode = self.scraper.give_store_postcode()
-        return nearest_postcode
-
-    def find_details_of_nearest(self) -> str:
-        nearest_store_data = self.scraper.give_store_details()
-        return nearest_store_data
+    def find_nearest_postcodes(self) -> list:
+        nearest_postcodes = self.scraper.get_nearest_postcodes()
+        return nearest_postcodes
 
     def insert_postcodes(self) -> None:
-        store_postcodes = []
-        for i in range(len(self.billboard_postcodes)):
-            self.index = i
-            nearest_postcode = self.find_postcode_of_nearest()
-            store_postcodes.append(nearest_postcode)
-            self.progress()
+        store_postcodes = self.find_nearest_postcodes()
         self.df["Nearest M.A.C Store Postcode"] = store_postcodes
-        self.df.to_excel(r'../M.A.C Billboard Data - w Nearest Postcodes.xlsx', index=False)
+        self.df.to_excel(r'../M.A.C Billboard Data - w Nearest Postcodes - Test.xlsx', index=False)
         return
 
-    def insert_details(self, s_name: bool = True, s_postcode: bool = True, s_number: bool = True,
-                       s_distance_away: bool = True) -> None:
-        store_postcodes = []
-        distances_away = []
-        store_tel_numbers = []
-        for i in range(len(self.billboard_postcodes)):
-            self.index = i
-            store_details = self.find_details_of_nearest()
-
-            detail_rows = store_details.split('\n')
-            # extracting postcode
-            row_withpostcode = detail_rows[2].split(' ')
-            postcode = f'{row_withpostcode[len(row_withpostcode) - 2]} {row_withpostcode[len(row_withpostcode) - 1]}'
-            store_postcodes.append(postcode)
-            # extracting_distance
-            row_withdistance = detail_rows[1].split(' ')
-            distance = f'{row_withdistance[2]} {row_withdistance[3]}'
-            distances_away.append(distance)
-            # extracting store telephone numbers
-            row_with_telnumbers = detail_rows[3].split(' ')
-            tel_numbers = ' '.join(row_with_telnumbers[8:])
-            store_tel_numbers.append(tel_numbers)
-            self.progress()
-
-        self.df['M.A.C Store Postcode'] = store_postcodes
-        self.df['M.A.C Store Tel Number'] = store_tel_numbers
-        self.df['Distance Away'] = distances_away
-        self.df.to_excel(r'../M.A.C Billboard Data - w M.A.C Store Data.xlsx', index=False)
-        return
+    # def insert_details(self, s_name: bool = True, s_postcode: bool = True, s_number: bool = True,
+    #                    s_distance_away: bool = True) -> None:
+    #     store_postcodes = []
+    #     distances_away = []
+    #     store_tel_numbers = []
+    #     for i in range(len(self.billboard_postcodes)):
+    #         self.index = i
+    #         store_details = self.find_details_of_nearest()
+    #
+    #         detail_rows = store_details.split('\n')
+    #         # extracting postcode
+    #         row_with_postcode = detail_rows[2].split(' ')
+    #         postcode = f'{row_with postcode[len(row_with_postcode) - 2]} {row_with_postcode[len(row_with_postcode) - 1]}'
+    #         store_postcodes.append(postcode)
+    #         # extracting_distance
+    #         row_with_distance = detail_rows[1].split(' ')
+    #         distance = f'{row_with_distance[2]} {row_with_distance[3]}'
+    #         distances_away.append(distance)
+    #         # extracting store telephone numbers
+    #         row_with_tel_numbers = detail_rows[3].split(' ')
+    #         tel_numbers = ' '.join(row_with_tel_numbers[8:])
+    #         store_tel_numbers.append(tel_numbers)
+    #         self.progress()
+    #
+    #     self.df['M.A.C Store Postcode'] = store_postcodes
+    #     self.df['M.A.C Store Tel Number'] = store_tel_numbers
+    #     self.df['Distance Away'] = distances_away
+    #     self.df.to_excel(r'../M.A.C Billboard Data - w M.A.C Store Data.xlsx', index=False)
+    #     return
 
 
 sample = BillBoardFinder()
-sample.insert_details()
+sample.insert_postcodes()
